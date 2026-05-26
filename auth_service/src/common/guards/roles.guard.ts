0ec443fbd9700e6087c89f13f29605d9e4b08287
Edwin Winarto <edwinwinartoa@gmail.com>
2026-05-26 15:38:24 +0700

    Add Role Authorization Guard

---
+++ auth_service/src/common/guards/roles.guard.ts
@@ -0,0 +1,33 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '../enums/role.enum';
+
+type RequestWithUser = {
+  user?: {
+    userId: number;
+    role: Role;
+  };
+};
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles?.length) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const userRole = request.user?.role;
+
+    return Boolean(userRole && requiredRoles.includes(userRole));
+  }
+}
